270a037c2e63ce2ee0f938401a36dca3db404cef
osw003 <ss06003@ues.edu.sv>
2020-11-18 23:00:52 -0600

    Asignacion03.{Barrios,García,Solórzano}

--- app/Http/Controllers/SistemaEmbebidoController.php
+++ app/Http/Controllers/SistemaEmbebidoController.php
@@ -14,6 +14,8 @@ class SistemaEmbebidoController extends Controller
     public function index()
     {
         //
+        $sistemaEmbebidos = SistemaEmbebido::all();
+        return response()->json($sistemaEmbebidos, 200);
     }
 
     /**
@@ -25,6 +27,24 @@ class SistemaEmbebidoController extends Controller
     public function store(Request $request)
     {
         //
+         //dd($request->all());
+         if(!isset($request->nombre)){
+            return response()->json(['error' -> 'El nombre no puede estar vacio'], 400)
+        }
+        if(!isset($request->edad)){
+            return response()->json(['error' -> 'La edad no puede estar vacio'], 400)
+        }
+
+        try{
+            $sistemaEmbebido = SistemaEmbebido::created([
+                'nombre' -> $request->nombre,
+                'edad' -> $request->edad
+            ]);
+        }catch(QueryException $th){
+            return response()->json(['error' -> 'no se pudo guardar en la base de datos'], 400);
+        }
+
+        return response()->json($sistemaEmbebido, 200);
     }
 
     /**
@@ -36,6 +56,13 @@ class SistemaEmbebidoController extends Controller
     public function show($id)
     {
         //
+        try{
+            $sistemaEmbebido = SistemaEmbebido::findOrFall($id);
+
+         }catch (ModelNotFoundException $th){
+             return response()->json(['error'=> 'No se encuentra el sistema embebido'],404)
+         }
+         return response()->json($sistemaEmbebido,200);
     }
 
     /**
@@ -48,6 +75,29 @@ class SistemaEmbebidoController extends Controller
     public function update(Request $request, $id)
     {
         //
+        dd($request->all());
+        try{
+            $sistemaEmbebido = SistemaEmbebido::findOrFall($id);
+
+         }catch (ModelNotFoundException $th){
+             return response()->json(['error'=> 'No se encuentra el sistema embebido'],404)
+         }
+
+         if(!isset($request->nombre) && !isset($request->edad)){
+            return response()->json(['date' -> 'No se envio informacion'],200);
+        }elseif(!isset($request->nombre)){
+            $sistemaEmbebido->edad $request->edad;
+            $sistemaEmbebido->save();
+        }elseif(!isset($request->edad)){
+            $sistemaEmbebido->nombre = $request->nombre;
+            $sistemaEmbebido->save();
+        }else{
+            $sistemaEmbebido->nombre = $request->nombre;
+            $sistemaEmbebido->edad = $request->edad;
+            $sistemaEmbebido->save();
+        }
+
+
     }
 
     /**
@@ -59,5 +109,16 @@ class SistemaEmbebidoController extends Controller
     public function destroy($id)
     {
         //
-    }
-}
+        try{
+            $sistemaEmbebido = SistemaEmbebido::findOrFall($id);
+
+         }catch (ModelNotFoundException $th){
+             return response()->json(['error'=> 'No se encuentra el sistema embebido'],404)
+         }
+         $sistemaEmbebido->delete();
+
+         return response()->json(['dat'=> 'Se eliminó correctamente',$sistemaEmbebido],200);
+
+
+
+
