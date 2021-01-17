8ce97e3eea59c0783177979983e05e8f48d97cba
VM17015 <VM17015@ues.edu.sv>
2021-01-16 22:47:15 -0600

    Limpieza de datos en los seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         $this->call(VM17015Seeder::class);
-        $this->call(MP19084Seeder::class);
-        $this->call(AL13013Seeder::class);
-        $this->call(GR09071Seeder::class);
-        $this->call(BU03002Seeder::class);
-        $this->call(SS06003Seeder::class);
+        // $this->call(MP19084Seeder::class);
+        // $this->call(AL13013Seeder::class);
+        // $this->call(GR09071Seeder::class);
+        // $this->call(BU03002Seeder::class);
+        // $this->call(SS06003Seeder::class);
     }
 }
--- database/seeds/VM17015Seeder.php
+++ database/seeds/VM17015Seeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\User;
+use App\Unidad;
+use App\TipoDato;
+use App\Componente;
+use App\SistemaEmbebido;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -20,5 +24,69 @@ class VM17015Seeder extends Seeder
             'email' => 'admin@gmail.com',
             'password' => Hash::make("admin"),
         ]);
+
+        SistemaEmbebido::create([
+            'user_id'=>1,
+            'nombre'=>"Sistema de temperatura y porcentaje de humedad",
+            'activo'=>true
+        ]);
+
+        SistemaEmbebido::create([
+            'user_id'=>1,
+            'nombre'=>"Sistema de medicion de Luz",
+            'activo'=>true
+        ]);
+
+        TipoDato::create([
+            'nombre'=>"Entero"
+        ]);
+        TipoDato::create([
+            'nombre'=>"Decimal"
+        ]);
+        TipoDato::create([
+            'nombre'=>"Cadena de texto"
+        ]);
+
+
+        Unidad::create([
+            'user_id'=>1,
+            'nombre'=>"Estandar",
+            'abreviacion'=>""
+        ]);
+        Unidad::create([
+            'user_id'=>1,
+            'nombre'=>"Grado Centigrado",
+            'abreviacion'=>"°C"
+        ]);
+        Unidad::create([
+            'user_id'=>1,
+            'nombre'=>"Porcentaje",
+            'abreviacion'=>"%"
+        ]);
+        Unidad::create([
+            'user_id'=>1,
+            'nombre'=>"Voltaje",
+            'abreviacion'=>"V"
+        ]);
+
+       Componente::create([
+        'sistema_embebido_id'=>1,
+        'tipo_dato_id'=>2,
+        'unidad_id'=>2,
+        'nombre'=>"Termometro"
+       ]);
+       Componente::create([
+        'sistema_embebido_id'=>1,
+        'tipo_dato_id'=>1,
+        'unidad_id'=>3,
+        'nombre'=>"Higrometro"
+       ]);
+
+       Componente::create([
+        'sistema_embebido_id'=>2,
+        'tipo_dato_id'=>1,
+        'unidad_id'=>1,
+        'nombre'=>"Fotoresistor"
+       ]);
     }
 }
